45984b47073cbd5ad335cfae5ef06009aac5d915
bjorn@dune.com <bjorn@dune.com>
2022-08-15 13:24:16 +0000

    added first model and updated config version

---
+++ models/dim_customers.sql
@@ -0,0 +1,56 @@
+with customers as (
+
+    select
+        ID as customer_id,
+        FIRST_NAME as first_name,
+        LAST_NAME as last_name
+
+    from default.jaffle_shop_customers_csv
+
+),
+
+orders as (
+
+    select
+        ID as order_id,
+        USER_ID as customer_id,
+        ORDER_DATE as order_date,
+        STATUS as status
+
+    from default.jaffle_shop_orders_csv
+
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
+
+),
+
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customers
+
+    left join customer_orders using (customer_id)
+
+)
+
+select * from final
